ab43dea75c1189cdfd42ecd9675398ddf41ef11e
Jonathan Swartz <swartz@pobox.com>
2011-03-29 17:15:22 -0700

    merge binaries into init_db.pl

--- bin/init_db.pl
+++ bin/init_db.pl
@@ -1,6 +1,13 @@
 #!/usr/bin/perl
 use Spectre::Script qw($root);
+use Spectre::Report;
 use IPC::System::Simple qw(run);
 
 unlink("$root/data/spectre.db");
 run("sqlite3 $root/data/spectre.db < $root/db/spectre.sql");
+my $dir = "$root/data/incoming";
+foreach my $archive_file ( glob("$dir/*.tar.gz") ) {
+    Spectre::Report->new_from_tap_archive($archive_file);
+}
+my $reports_count = Spectre::Report::Manager->get_reports_count;
+die "load failed -- reports_count = $reports_count" unless $reports_count == 6;
--- lib/Spectre/DB/Object.pm
+++ lib/Spectre/DB/Object.pm
@@ -1,5 +1,6 @@
 package Spectre::DB::Object;
 use Spectre qw($root);
+use Spectre::DB;
 use strict;
 use warnings;
 use base qw(Rose::DB::Object);
